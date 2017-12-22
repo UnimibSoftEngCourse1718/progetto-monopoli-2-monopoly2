64400dc28cad2cff7327edd19c23449a5aa4d7e4
DanieleTamborrino <d.tamborrino27@gmail.com>
2017-12-22 11:43:45 +0100

    Riposizionato il campo, aggiunto il font e inserito un .gitignore (Non sincronizza più il layout delle finestre di unity, così evitiamo un errore ogni volta che apriamo unity)

---
+++ Unity/NewMonopoly/Assets/Script/Casella.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Casella : MonoBehaviour {
+    public GameObject Proprietario;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
