377fea668488351762e4df07265b94da4e100b65
MarcoMartinelli32 <m.martinelli32@campus.unimib.it>
2018-01-19 00:23:18 +0100

    Aggiunta animazione cambio giocatore
    Ho aggiunto una semplice animazione che va a evidenziare il giocatore attivo in quel momento, e quando si passa il turno, viene cambiata la evidenziazione , lo script è nel file "passa"
    p.s. sarà da gestire quando gestiremo il numero di giocatori variabili (ho usato un array statico apposta)

--- Unity/NewMonopoly/Assets/Script/passa.cs
+++ Unity/NewMonopoly/Assets/Script/passa.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class passa : MonoBehaviour {
 
+    private int contatoregiocatoreattivo = 1;
+    public GameObject[] panelList = new GameObject[5];
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +16,24 @@ public class passa : MonoBehaviour {
 	void Update () {
 		
 	}
+
+
+    public void passaturno()
+    {
+        if (contatoregiocatoreattivo == 6)
+        {
+            contatoregiocatoreattivo = 0;
+            panelList[contatoregiocatoreattivo].active = true;
+            panelList[5].active = false;
+            contatoregiocatoreattivo++;
+        }
+        else
+        {
+            panelList[contatoregiocatoreattivo].active = true;
+            panelList[contatoregiocatoreattivo - 1].active = false;
+            contatoregiocatoreattivo++;
+        }  
+
+        
+    }
 }
