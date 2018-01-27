958de2ff3677e87e79b7683afe5ce3fed93198f0
MarcoMartinelli32 <33154616+MarcoMartinelli32@users.noreply.github.com>
2018-01-27 14:23:03 +0100

    Piccola modifica in trattativa
    Aggiunto un semicontrollo sui soldi e un messaggio di errore nel caso non si abbiano abbastanza soldi

---
+++ Unity/NewMonopoly/Assets/Script/Offri.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Offri : MonoBehaviour {
+
+    /*public Giocatore giocatore1;
+    public Giocatore giocatore2;
+    public Giocatore giocatore3;
+    public Giocatore giocatore4;
+    public Giocatore giocatore5;
+    public Giocatore giocatore6;*/
+
+    // Use this for initialization
+    void Start() {
+       // if (int.Parse(GameObject.Find("Inserisci_Soldi1").GetComponent<Text>().text) > CurrentPlayerID.Soldi; 
+            GameObject.Find("Nonhaisoldi").active = false;
+
+       // if (int.Parse(GameObject.Find("Inserisci_Soldi2").GetComponent<Text>().text) > )
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void unload()
+    {
+        GameObject.Find("Nonhaisoldi").active = false;
+    }
+}
