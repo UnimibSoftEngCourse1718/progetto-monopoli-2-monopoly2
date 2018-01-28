b5b31bdad2d55dfc14b9c73dfdd6277edabc8506
JulianPaglione <j.paglione@campus.unimib.it>
2018-01-28 16:42:32 +0100

    aggiunta controllo passaggio turno

--- Unity/NewMonopoly/Assets/Script/RANDOM.cs
+++ Unity/NewMonopoly/Assets/Script/RANDOM.cs
@@ -8,20 +8,18 @@ public class RANDOM : MonoBehaviour {
 
     public Text[] text;
     public int risultato = 0; // Ogni volta che si tira si azzera il punteggio dei dadi
-<<<<<<< HEAD
-    public int doppio = 0;
 
-=======
- 
->>>>>>> 958de2ff3677e87e79b7683afe5ce3fed93198f0
+    
+    public int doppio = 0;
+    public StateController theStateController;
+
     // Use this for initialization
     void Start () {
 
-        theStateController = GameObject.FindObjectOfType<StateController>();
         
     }
     // aggiungo lo statecontroller per controllare lo stato del turno e gestire  il giocatore di turno 
-   public StateController theStateController;
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -43,11 +41,12 @@ public class RANDOM : MonoBehaviour {
         }
         text[0].text = UnityEngine.Random.Range(1, 7).ToString(); //Il 7 è escluso
         text[1].text = UnityEngine.Random.Range(1, 7).ToString();
-<<<<<<< HEAD
+
         if (text[0].text.Equals(text[1].text)) doppio++;
-=======
+
+        theStateController.IsDoneRolling = true;//tiro effettivo
      
->>>>>>> 958de2ff3677e87e79b7683afe5ce3fed93198f0
+
         risultato = int.Parse(text[0].text) + int.Parse(text[1].text);
         text[2].text = risultato.ToString();
         theStateController.DiceTotal = risultato;
--- Unity/NewMonopoly/Assets/Script/giocatore.cs
+++ Unity/NewMonopoly/Assets/Script/giocatore.cs
@@ -9,6 +9,8 @@ public class Giocatore : MonoBehaviour {
     public int contatorePrigione;
     public bool uscitaDiPrigione;
     public int soldi;
+    public StateController controller;
+    bool isAnimating;
 
 	// Use this for initialization
 	void Start () {
@@ -31,7 +33,7 @@ public class Giocatore : MonoBehaviour {
 	}
 
 
-    void SetNewTargetPosition(Vector3 pos)
+    public void SetNewTargetPosition(Vector3 pos)
     {
 
         targetPosition = pos;
@@ -41,7 +43,10 @@ public class Giocatore : MonoBehaviour {
      private void OnMouseUp()
     {
         int spazio = int.Parse(GameObject.Find("Risultato Dadi").GetComponent<Text>().text);
-       
+
+        
+        if (controller.IsDoneClicking == true) return; // ho già tirato , mo basta
+
         Casella arrivo = partenza;
         for(int i = 0; i < spazio; i++)
         {
@@ -56,6 +61,8 @@ public class Giocatore : MonoBehaviour {
             }
         if (arrivo == null) return;
 
+        controller.IsDoneClicking = true;
+        this.isAnimating = true;
         SetNewTargetPosition(arrivo.transform.position);
         partenza = arrivo;
     }
