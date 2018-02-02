63998debff0b70cdf058875438b01be14537cce7
DanieleTamborrino <d.tamborrino27@gmail.com>
2018-02-02 14:50:15 +0100

    Corretto bug sui segnalini delle proprietà

--- Unity/NewMonopoly/Assets/Script/CasellaAcquistabile.cs
+++ Unity/NewMonopoly/Assets/Script/CasellaAcquistabile.cs
@@ -11,47 +11,50 @@ public class CasellaAcquistabile : Casella {
 
     public void CambioProprietario()
     {
-        if (proprietario == null)
+        if (segnalinoProprietario != null)
         {
             DestroyImmediate(segnalinoProprietario);
-            return;
         }
+        
+        if (proprietario != null)
+        {
 
-        if (this as Terreno != null)
-        {
-            Terreno terreno = this as Terreno;
-            terreno.RimuoviEdifici();
-        }
+            if (this as Terreno != null)
+            {
+                Terreno terreno = this as Terreno;
+                terreno.RimuoviEdifici();
+            }
 
-        segnalinoProprietario = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        segnalinoProprietario.GetComponent<Collider>().enabled = false;
-        segnalinoProprietario.transform.position = this.transform.position;
-        segnalinoProprietario.GetComponent<Renderer>().material = proprietario.GetComponent<Renderer>().material;
+            segnalinoProprietario = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            segnalinoProprietario.GetComponent<Collider>().enabled = false;
+            segnalinoProprietario.transform.position = this.transform.position;
+            segnalinoProprietario.GetComponent<Renderer>().material = proprietario.GetComponent<Renderer>().material;
 
-        int i = int.Parse(this.name);
-        if (i < 11)
-        {
-            // caselle sotto
-            segnalinoProprietario.transform.localScale = new Vector3(8, 1, 2);
-            segnalinoProprietario.transform.position += new Vector3(0, 0, -7);
-        }
-        else if (i < 21)
-        {
-            // caselle sinistra
-            segnalinoProprietario.transform.localScale = new Vector3(2, 1, 8);
-            segnalinoProprietario.transform.position += new Vector3(-7, 0, 0);
-        }
-        else if (i < 31)
-        {
-            // caselle sopra
-            segnalinoProprietario.transform.localScale = new Vector3(8, 1, 2);
-            segnalinoProprietario.transform.position += new Vector3(0, 0, 7);
-        }
-        else
-        {
-            // caselle destra
-            segnalinoProprietario.transform.localScale = new Vector3(2, 1, 8);
-            segnalinoProprietario.transform.position += new Vector3(7, 0, 0);
+            int i = int.Parse(this.name);
+            if (i < 11)
+            {
+                // caselle sotto
+                segnalinoProprietario.transform.localScale = new Vector3(8, 1, 2);
+                segnalinoProprietario.transform.position += new Vector3(0, 0, -7);
+            }
+            else if (i < 21)
+            {
+                // caselle sinistra
+                segnalinoProprietario.transform.localScale = new Vector3(2, 1, 8);
+                segnalinoProprietario.transform.position += new Vector3(-7, 0, 0);
+            }
+            else if (i < 31)
+            {
+                // caselle sopra
+                segnalinoProprietario.transform.localScale = new Vector3(8, 1, 2);
+                segnalinoProprietario.transform.position += new Vector3(0, 0, 7);
+            }
+            else
+            {
+                // caselle destra
+                segnalinoProprietario.transform.localScale = new Vector3(2, 1, 8);
+                segnalinoProprietario.transform.position += new Vector3(7, 0, 0);
+            }
         }
     }
 }
